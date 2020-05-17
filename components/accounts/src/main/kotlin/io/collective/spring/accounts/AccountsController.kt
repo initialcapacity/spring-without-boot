f7b9669875481876aa3707ba4da96111432ab1d8
Tyson Gern <tyson@gern.org>
2020-05-17 13:08:40 -0600

    Remove unnecessary @Inject annotations

--- components/accounts/src/main/kotlin/io/collective/spring/accounts/AccountsController.kt
+++ components/accounts/src/main/kotlin/io/collective/spring/accounts/AccountsController.kt
@@ -2,10 +2,9 @@ package io.collective.spring.accounts
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import javax.inject.Inject
 
 @RestController
-class AccountsController @Inject constructor(private val gateway: AccountsDataGateway) {
+class AccountsController constructor(private val gateway: AccountsDataGateway) {
 
     @RequestMapping("/api/accounts")
     fun getAccounts() = gateway.getAccounts()
